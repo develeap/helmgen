8d27424408f867266a28a5253cd011206009c4e7
Shaked Dotan <shaked.dotan@develeap.com>
2023-08-25 02:45:00 +0300

    small syntax fix

--- app/helmgen/bs/deployment_compare.py
+++ app/helmgen/bs/deployment_compare.py
@@ -3,11 +3,10 @@ import yaml
 from termcolor import colored
 
 def extract_remote_deployment_info(deployment_name):
+    extracted_data = []
     try:
         remote_deployment_output = subprocess.check_output(["kubectl", "get", "deploy", deployment_name, "-oyaml"]).decode("utf-8")
         remote_deployment_data = yaml.safe_load(remote_deployment_output)
-
-        extracted_data = []
 
         for item in remote_deployment_data.get("spec", {}).get("template", {}).get("spec", {}).get("containers", []):
             extracted_data.append({
@@ -25,27 +24,24 @@ def extract_remote_deployment_info(deployment_name):
                     }
                 }
             })
-
         return extracted_data
 
     except subprocess.CalledProcessError as e:
         print(f"Error retrieving remote deployment: {e}")
         return None
 
-def main():
+def compare():
     try:
         with open('config-minimal.yml', 'r') as config_file:
             config_data = yaml.safe_load(config_file)
 
         for deployment in config_data.get('deployments', []):
             deployment_name = deployment['name']
-
             remote_deployment_info = extract_remote_deployment_info(deployment_name)
 
             if not remote_deployment_info:
                 print(f"Failed to retrieve remote deployment information for {deployment_name}.")
                 continue
-
             local_deployment_data = deployment.get('spec', {}).get('template', {}).get('spec', {}).get('containers', [])
 
             print(f"\nDifferences for deployment: {deployment_name}\n")
@@ -93,4 +89,4 @@ def main():
         print(e)
 
 if __name__ == "__main__":
-    main()
+    compare()
